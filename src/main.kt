81d666757e9549a6dbab19d656c54893dc42477f
TerraBoii <kostyaborisenko05@gmail.com>
2022-10-31 19:01:13 +0300

    Done

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,29 @@
 package com.terraboii.ideal_root
 
+import kotlin.system.exitProcess
+
+fun new(): Int = (1..99).random()
+
 fun main() {
-    println("Hello")
+    while (true) {
+        var root = new()
+        while (true) {
+            println("Try to find square root of ${root * root}, or enter 0 to exit.")
+            print("Answer -> ")
+            var answer = readln().toIntOrNull()
+            if (answer is Int) {
+                when {
+                    answer == 0 -> {
+                        println("Exiting...")
+                        exitProcess(0)
+                    }
+                    answer == root -> {
+                        println("Congratulations.\n")
+                        break
+                    }
+                }
+            }
+
+        }
+    }
 }
