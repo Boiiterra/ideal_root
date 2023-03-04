c1b69081bf6c7d880fa6d29b849afd1354006680
TerraBoii <kostyaborisenko05@gmail.com>
2023-03-04 12:21:43 +0300

    Updated script

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kotlinc ./src/main.kt -include-runtime -d ./src/out/main.jar
-java -jar ./src/out/main.jar
+kotlinc ./src/main.kt -include-runtime -d ./src/out/main.jar && java -jar ./src/out/main.jar && rm ./src/out/main.jar
